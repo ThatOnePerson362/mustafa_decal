a9e3fe5563c70a94f0bb1c85bc5d8fbaa3a482b9
Mustafa Abuzahriyeh <mustafaabuzahriyeh@Mustafas-MacBook-Pro.local>
2025-02-25 22:25:18 -0800

    Adding homework2

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,22 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+    # To navigate directly to ~/Desktop, I would run the command cd ~/Desktop
 # 2) What does ~/ mean?
-# 3) What's the difference between and abolute and a relative path in your terminal?
+    #  ~/ is the indicator for an absolute path.
+# 3) What's the difference between and absolute and a relative path in your terminal?
+    # An absolute path allows you to change directories with respect to your home directory, whereas a relative path lets you create a path to change directories starting from any directory
 # 4) What command would bring you back to your home directory?
-# 5) If you called rm ./ in your current working directory, what would it do? 
+    # cd 
+# 5) If you called rm ./ in your current working directory, what would it do?
+    # It would get rid of everything in that directory (Source: https://gist.github.com/bradtraversy/cc180de0edee05075a6139e42d5f28ce), but I did try running that in the terminal and the following was returned: rm: "." and ".." may not be removed.
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+    # Calling "git add" begins the process of adding a file or folder to the remote repository and puts it in a "staging area" (Source: Class 4 lecture slides). "git commit" will show you what changes you are making and save them to the local repository. "git push" sends what you are adding to the remote repository.
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+    # I am not sure what the exact message would be. In lecture, when "git status" was used after completing the process of moving a directory to the remote repository, it showed a message that there was "nothing to commit" (i.e. the local and remote repositories were identical). If you ran "git add" and then "git status", it would probably show the name of the file that you have yet to commit.
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+    # I can't recall any super frustrating errors or bugs off the top of my head. My coding intuition is lacking, so I usually do not even know what to start with for troubleshooting. I think in the terminal I have had to correct spelling errors or use "master" instead of "main" for the "git push" command.
 # 9) How does cloning a repository differ from pulling from a repository?
+    # cloning a repository is when you take a repository from GitHub and put it onto your computer (it was not there beforehand). Pulling from a repository is when you make changes/updates to it (The example used in lecture was downloading a game is like cloning a repositiory and any future updates you make to that game are similar to pulling from a repository.).
 # 10) Tell me a fun fact!
+    # Many words in Arabic can be breaken down into three letter roots.
+
+# other sources used: GitHub continued lecture video, https://education.github.com/git-cheat-sheet-education.pdf
